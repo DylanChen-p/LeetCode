e6a9650cf05dab8bd5b4972289bf97927c8836e6
DylanChen-p <117508205+DylanChen-p@users.noreply.github.com>
2023-08-10 22:56:38 +0800

    48 & 1304 updated

--- leetcode_1304/src/Main.java
+++ leetcode_1304/src/Main.java
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
 
 Given an integer n, return any array containing n unique integers such that they add up to 0.
- */
+*/
 
 public class Main {
     public int[] sumZero(int n) {
